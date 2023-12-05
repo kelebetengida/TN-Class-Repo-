dc0a49c5c9e39168e61ceb327b8c6cb9dc013d95
Kelebet Engida <engida398@gmail.com>
2023-12-05 12:23:26 -0800

    All Week 4 class Notes

---
+++ Class Notes/Week 4/Day 3/loops.js
@@ -0,0 +1,52 @@
+
+// ///////////////////////////////for Loop///////////////////////////////////////////////
+// // For loop structure
+// for(initialization, condition, increment/decrement){
+//     // code goes here
+//   }
+
+
+/////////////////////////////////////////while loop///////////////////////////////////////
+
+
+
+//////////////////////////////////////// do while loop////////////////////////////////////
+
+
+
+// ///////////////////////////// for of loop//////////////////////////////////////////////
+// for (const element of arr) {
+//     // code goes here
+//   }
+
+
+
+// ///////////////////////////////////////////break/////////////////////////////////////
+//Break is used to interrupt a loop.
+
+
+
+
+/////////////////////////////////////// continue///////////////////////////////////////
+//We use the keyword continue to skip a certain iterations.
+
+
+
+
+
+
+
+
+//..........................................To do .......................................
+// Use for loop to iterate from 0 to 100 and print only even numbers
+
+// Use for loop to iterate from 0 to 100 and print only odd numbers
+
+//Use for loop to iterate from 0 to 100 and print the sum of all numbers.
+
+
+
+///////////////////////////////////////////Challenge///////////////////////////////////////
+let countries=["ALBANIA", "BOLIVIA", "CANADA", "DENMARK", "ETHIOPIA", "FINLAND", "GERMANY", "HUNGARY", "IRELAND", "JAPAN", "KENYA"]
+//Using the above countries array, create an array for countries length'.
+//Expected outcome is [7, 7, 6, 7, 8, 7, 7, 7, 7, 5, 5]
