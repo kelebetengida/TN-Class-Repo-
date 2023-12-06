6e8e0f92719ac77ba64b496f861ec172953091d1
Kelebet Engida <engida398@gmail.com>
2023-12-05 18:22:51 -0800

    Updated arrays

--- Class Notes/Week 4/Day 2/arrays.js
+++ Class Notes/Week 4/Day 2/arrays.js
@@ -3,13 +3,16 @@
 ///////////////////////////////////////////////////////////////////////////////////////////////
 // How to create an empty array. 
 // This the most recommended way to create an empty list. Here is a general syntax
-
+// let arr=[]
+// console.log(arr)
 
 
 
 //////////////////////////////////////////////////////////////////////////////////////////////
 // How to create an array with values
-
+let arr=["Denmark", "Swedan", "Norway", 7, 8, true, false, null, undefined, [ "banana", "orange"], {color:"blue", make:"Honda", awd:false}]
+console.log("this is a random list of items:", arr)
+console.log("This is how many are inside the arr list:", arr.length)
 
 //.........................................To Do..............................................
 //Create an array with values and then console log the list and its length  
@@ -27,8 +30,8 @@
 // /////////////////////////////////////////////////////////////////////////////////////////////
 // Creating an array using split
 let js = 'JavaScript'
-
-
+const charOfJs=js.split("")
+console.log(charOfJs)
 
 //.........................................To Do..............................................
 //For the two strings use a split method to conosole.log and see the list of arrays
@@ -44,7 +47,11 @@ let txt =
 
 const fruits = ['banana', 'orange', 'mango', 'lemon']
 //index of Fruit////0/////////1////////2////////3////
+let oneFruit=fruits[2]
+console.log(oneFruit)
 
+let lastIndex=fruits.length-1
+console.log(fruits[lastIndex])
 
 //.........................................To Do..............................................
 //console how many are in  my shoping bag, just accsess and pring potato, Milk and sugar. Replace Avocado with Onion
@@ -66,8 +73,9 @@ const shoppingCart = [
 //////////////////////////////////////////////////////////////////////////////////////////////
 // Modifying array element
 const numbers = [1, 2, 3, 4, 5]
-
-
+/////////////////0//1//2
+numbers[1]=12
+console.log(numbers)
 
 
 
