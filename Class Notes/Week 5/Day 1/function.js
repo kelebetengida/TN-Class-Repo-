d05a18cafdbda88ea9e5f58d07afe2d5afa89e40
Kelebet Engida <engida398@gmail.com>
2023-12-11 18:11:55 -0800

    Updated Function

--- Class Notes/Week 5/Day 1/function.js
+++ Class Notes/Week 5/Day 1/function.js
@@ -1,12 +1,58 @@
 // /////////////////////////////////Functions//////////////////////////////////////////////////
-
+//Buliding block of your JS code
+//it is a reusable block of code allow you to perform a specific task
 // Function Declaration
-// Function without a parameter and return
-// Function returning value
-// Function with a parameter
-// Function with two parameters
-// Function with many parameters
+function functionName(){
+    //code goes here
+}
 
+functionName() //calling the function into action 
+
+function greetingsGenerator(name){
+    console.log("hello"+ " "+ name)
+}
+
+greetingsGenerator("Kelebet")
+
+// Function without a parameter and return
+
+// function add(){
+//     let num1=5;
+//     let num2=5;
+//     let sum=num1+num2
+//     console.log(sum)
+// }
+
+// add()
+// Function returning value
+function add(){
+    let num1=5;
+    let num2=5;
+    let sum=num1+num2
+    return sum
+}
+
+console.log(add())
+
+
+// Function with a parameter
+// function greetingsGenerator(name){
+//     console.log("hello"+ " "+ name)
+// }
+
+// greetingsGenerator("Kelebet")
+// Function with two parameters
+// function adding(num1, num2){
+//     console.log(num1+num2)
+// }
+
+// adding(555, 89)
+// Function with many parameters
+function adding(num1, num2, num3, num4){
+    console.log(num1+num2+num3+num4)
+}
+
+adding(555, 89,45,48)
 //......................................To Do................................................
 // Declare a function fullName and it print out your full name.
 
@@ -21,3 +67,7 @@
 
 
 // Declare a function name capitalizeArray. It takes array as a parameter and it returns the - capitalizedarray.
+
+// ["us", "canada","barcelona"]
+
+// return ["US", "CANADA", "BARCELONA"]
