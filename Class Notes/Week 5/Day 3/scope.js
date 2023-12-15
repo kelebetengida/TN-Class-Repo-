28c056dcf9d8229a56ad1096ee3abf6c285c5a2f
Kelebet Engida <engida398@gmail.com>
2023-12-14 16:28:43 -0800

    Scope activity

--- Class Notes/Week 5/Day 3/scope.js
+++ Class Notes/Week 5/Day 3/scope.js
@@ -3,3 +3,13 @@
 // Global scope
 // Local scope
 
+
+//.....................................To Do.................................................
+//ES6 in FCC
+//Compare Scopes of the var and let Keywords
+
+
+//Basic JS FCC
+// Global Scope and Functions
+// Local Scope and Functions
+// Global vs. Local Scope in Functions
