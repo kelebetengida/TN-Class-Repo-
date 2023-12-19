f5561752abc91b932fb75547e393b91069c5ddee
Kelebet Engida <engida398@gmail.com>
2023-12-18 17:39:37 -0800

    Object notes

--- Class Notes/Week 6/Day 1/Objects.js
+++ Class Notes/Week 6/Day 1/Objects.js
@@ -1,9 +1,43 @@
-// 📔 Object
+//📔  Object
+
 // Creating an empty object
+
+const person={
+    hight:"short",
+    eyeColor:"Brown",
+    hairColor:"Black",
+    age:45,
+    isMaried:false
+}
+
+const Kelebet={
+    jobTitle:"Coding instructor",
+    greatAtSpelling: false, 
+    eyeColor:"Brown",
+    age:26,
+    skills:["HTML", "CSS", "JS", "Python", "React", "Node", "Express", "MangoDB"],
+    canSwim:null,
+    canDrive:true,
+    Honda:{
+        color:"Green",
+        make:"Accord",
+    }
+}
 // Creating an objecting with values
 // Getting values from an object
+
+console.log(Kelebet)
+console.log(Kelebet.age)
+console.log(Kelebet["age"])
+
+// console.log(Kelebet.jobTitle)
+// console.log(Kelebet.skills)
+console.log(Kelebet.Honda.color)
+console.log(Kelebet["Honda"]["color"])
+
 // Creating object methods
 // Setting new key for an object
+
 // Object Methods
 // Getting object keys using Object.keys()
 // Getting object values using Object.values()
@@ -11,7 +45,7 @@
 // Checking properties using hasOwnProperty()
 
 //..........................................To Do............................................
-// Create an empty object called 
+// Create an empty object called dog
 
 
 
