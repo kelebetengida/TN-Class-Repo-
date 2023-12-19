1d1affcc19c24d0bab4ff759b49c88b12ee11783
Kelebet Engida <engida398@gmail.com>
2023-12-18 16:27:53 -0800

    Week 6

---
+++ Class Notes/Week 6/Day 1/Objects.js
@@ -0,0 +1,26 @@
+// 📔 Object
+// Creating an empty object
+// Creating an objecting with values
+// Getting values from an object
+// Creating object methods
+// Setting new key for an object
+// Object Methods
+// Getting object keys using Object.keys()
+// Getting object values using Object.values()
+// Getting object keys and values using Object.entries()
+// Checking properties using hasOwnProperty()
+
+//..........................................To Do............................................
+// Create an empty object called 
+
+
+
+// Print the the dog object on the console
+
+
+
+// Add name, legs, color, age and bark properties for the dog object. The bark property is a method which return woof woof
+
+
+
+// Get name, legs, color, age and bark value from the dog object
