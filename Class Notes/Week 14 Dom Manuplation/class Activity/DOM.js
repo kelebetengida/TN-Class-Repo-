92f427b92040367666c9aa299a3d4ed50be9b386
kelebetengida <engida398@gmail.com>
2024-03-18 16:24:57 -0700

    Dom manuplation

---
+++ Class Notes/Week 14 Dom Manuplation/class Activity/DOM.js
@@ -0,0 +1,22 @@
+// ////////////////////////////DOM Manuplation Part 2///////////////////////////////////
+// Creating an Element
+
+
+
+// Creating elements
+
+
+
+
+// Appending child to a parent element
+
+
+
+
+// Removing a child element from a parent node
+
+
+
+
+
+//////////////////////////////Class Activity See the Mini Project/////////////////////////
