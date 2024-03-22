07ed7f722a9a64750d31159b9393338c74752a08
kelebetengida <engida398@gmail.com>
2024-03-21 18:58:23 -0700

    Update

--- Class Notes/Week 15 Event Listner/Class demo EventListner/Class Demo/Click Event/DOM.js
+++ Class Notes/Week 15 Event Listner/Class demo EventListner/Class Demo/Click Event/DOM.js
@@ -1,13 +1,17 @@
 /////////////////////////////////////////////////////////////////////////////////////////////
 //////////////////////////////// Event Listeners//////////////////////////////////////////
-// selectedElement.addEventListener('eventlistner', function(e) {
-//     // the activity you want to occur after the event will be in here
-//   })
-//   // or
+let selectedElement=document.querySelector("button")
+selectedElement.addEventListener('click', function() {
+    console.log("Hey You just clicked a button")
+  })
+  // or
   
-//   selectedElement.addEventListener('eventlistner', e => {
-//     // the activity you want to occur after the event will be in here
-//   })
+  selectedElement.addEventListener('eventlistner', e => {
+    // the activity you want to occur after the event will be in here
+  })
+
+// Evernt listner Video 
+// https://www.youtube.com/watch?v=XF1_MlZ5l6M
 
 
 // Click
