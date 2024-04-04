d1b9c6822ee0b6acd5a70b33a75020d87643a5fa
kelebetengida <engida398@gmail.com>
2024-04-03 18:50:58 -0700

    Updates

--- Class Notes/Week 17 Class OOP/Class Demos/DOM.js
+++ Class Notes/Week 17 Class OOP/Class Demos/DOM.js
@@ -30,28 +30,61 @@ const apples3 = {
 
 class Apples {
     // Class Constructor
-    constructor(color, types, taste, size){
-        this.color=color
-        this.types=types
-        this.taste=taste
-        this.size=size
-    }
+    constructor(color, types, taste, size) {
+        this.color = color
+        this.types = types
+        this.taste = taste
+        this.size = size
+        // Properties with initial value
+        this.count=0
+        this.location=[]
 
+    }
+    // Default values with constructor
+    // constructor(color="red", types"Honey Crisp", taste="Sweet", size="small") {
+    //     this.color = color
+    //     this.types = types
+    //     this.taste = taste
+    //     this.size = size
+    // }
+    
+    // Class methods
+    get apleType(){
+        const name= this.types
+        return name
+    }
+    // getter
+    get getAppleCount(){
+        return this.appleCount
+    }
+    get getAppleLocation(){
+        return this.location
+    }
+    // setter
+    set appleCount(count){
+        this.count+=count
+    }
+    set appleLocation(location){
+        this.location.push(location)
+    }
 }
 
-const apple1 = new Apples("red","Golden Delicious","Mid Sweet","small")
-const apple2 = new Apples("redGold MIx","Fuji","Sweet","Large")
+const apple1 = new Apples("red", "Golden Delicious", "Mid Sweet", "small")
+const apple2 = new Apples("redGold MIx", "Fuji", "Sweet", "Large")
+apple1.appleCount=8
+apple1.appleLocation="Atlanta"
+apple2.appleLocation="Nashvil"
+apple2.appleLocation="Chatnooga"
+
+apple2.appleCount=3
+console.log(Apples)
 console.log(apple1)
 console.log(apple2)
 
 
 
 
-// Default values with constructor
-// Class methods
-// Properties with initial value
-// getter
-// setter
+
 // Static method
 // Inheritance
 // Overriding methods
