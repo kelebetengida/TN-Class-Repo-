76f972d6eb6c37e0b384885db9eaba063cb8eef9
kelebetengida <engida398@gmail.com>
2024-04-03 17:29:38 -0700

    Final project

--- Class Notes/Week 17 Class OOP/Class Activity/DOM.js
+++ Class Notes/Week 17 Class OOP/Class Activity/DOM.js
@@ -1,9 +1,19 @@
 //Create different car objects
 
+class Car{
+    constructor(make, model, year, title, color){
+        this.make=make
+        this.model=model
+        this.year=year
+        this.title=title
+        this.color=color
+    }
+}
 
+let honda= new Car("honda", "Accord", 2024, true, "silver")
+console.log(honda)
 
-
-// Create an Animal class. The class will have name, age, color, legs properties and create different methods
+// Create an Animal class. The class will have name, age, color, legs properties and create different methods And Make sure to use a getter and setter
 
 
 
