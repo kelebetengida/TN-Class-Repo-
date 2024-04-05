5a92c2f4d92f42120af03ebec30c7cb22feccba6
kelebetengida <engida398@gmail.com>
2024-04-04 17:39:50 -0700

    Sub class and inheritance topic

--- Class Notes/Week 17 Class OOP/Class Activity/DOM.js
+++ Class Notes/Week 17 Class OOP/Class Activity/DOM.js
@@ -1,17 +1,46 @@
 //Create different car objects
 
 class Car{
-    constructor(make, model, year, title, color){
+    constructor(make, model, year, enginesize,brakesPrice, navigation){
         this.make=make
         this.model=model
         this.year=year
-        this.title=title
-        this.color=color
+        this.brakesPrice=brakesPrice
+        this.enginesize=enginesize
+        this.navigation=navigation
     }
 }
 
-let honda= new Car("honda", "Accord", 2024, true, "silver")
-console.log(honda)
+
+class Suv extends Car{
+    constructor(make, model, year, enginesize,brakesPrice, navigation,awd, numberOfPassengers, towCampacity ){
+        super(make, model, year, enginesize,brakesPrice, navigation)
+        this.awd=awd
+        this.numberOfPassengers=numberOfPassengers
+        this.towCampacity=towCampacity
+    }
+}
+
+const hondaPilot=new Suv("Honda", "Pilot", 2015, "V6", 455, true, true, 7, 50)
+
+class PickupTruck extends Car{
+    constructor(make, model, year, enginesize,brakesPrice, navigation, engineType, towCampacity){
+        super(make, model, year, enginesize,brakesPrice, navigation)
+        this.engineType=engineType
+        this.towCampacity=towCampacity
+    }
+}
+
+
+const f150=new PickupTruck("Ford", "f150", 2011, "V8", 500, true, "regular Engine", 1.5)
+
+
+
+
+
+
+
+
 
 // Create an Animal class. The class will have name, age, color, legs properties and create different methods And Make sure to use a getter and setter
 
