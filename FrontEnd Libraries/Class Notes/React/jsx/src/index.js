bc9d7606d43731181a1f59d2cf438b4a2b97e46f
kelebetengida <engida398@gmail.com>
2024-04-25 16:32:58 -0700

    React

---
+++ FrontEnd Libraries/Class Notes/React/jsx/src/index.js
@@ -0,0 +1,9 @@
+import React from "react"
+import ReactDOM from "react-dom"
+
+
+const jsxElement= <h1>Welcome to day 1 of React</h1>
+const rootElement=document.getElementById("root")
+
+
+ReactDOM.render(jsxElement, rootElement )
