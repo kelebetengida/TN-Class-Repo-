bbc89eff9ee679ef2c4a06ed5bcddb618f0fc09f
kelebetengida <engida398@gmail.com>
2024-05-06 16:23:57 -0700

    Props

---
+++ FrontEnd Libraries/Class Notes/React/Props/src/index.js
@@ -0,0 +1,25 @@
+// Props in Functional Component
+
+
+
+// What is props?
+// Props object
+// Different data type props
+// String props type
+// Number props type
+// Boolean props type
+// Array props type
+// Object props type
+// Function prop types
+// Destructuring props
+// propTypes
+// defaultProps
+
+
+
+
+// What is props in a React component ?
+// How do you access props in a React component ?
+// What data types can we pass as props to components ?
+// What is a propTypes?
+// What is a default propTypes?
