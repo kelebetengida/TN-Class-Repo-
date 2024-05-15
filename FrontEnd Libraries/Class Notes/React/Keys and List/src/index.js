66236fa8e67fd6d2d5f971f72c6cdf85265090be
kelebetengida <engida398@gmail.com>
2024-05-14 17:47:37 -0700

    Keys and list

--- FrontEnd Libraries/Class Notes/React/Keys and List/src/index.js
+++ FrontEnd Libraries/Class Notes/React/Keys and List/src/index.js
@@ -4,13 +4,13 @@
 // Mapping array of objects
 // Key in mapping arrays
 
-import React from "react"
-import ReactDOM from "react-dom"
+// import React from "react"
+// import ReactDOM from "react-dom"
 
-//Create a Numbers Component 
+// //Create a Numbers Component 
 
 // const Numbers = ({number}) => {
-//     const list=number.map((number)=><li>{number}</li>)
+//     const list=number.map((number)=><li key={number}>{number}</li>)
 //     return list
 // }
 
@@ -28,10 +28,10 @@ import ReactDOM from "react-dom"
 //         </div>
 //     )
 // }
-//const name=["Kelebet", "Victoria", "Phillip"]
+// const name=["Kelebet", "Victoria", "Phillip"]
 
 // const Names = ({ name }) => {
-//     const list = name.map((eachName) => <li>{eachName}</li>)
+//     const list = name.map((eachName) => <li key={eachName}>{eachName}</li>)
 //     return list
 // }
 
@@ -55,35 +55,70 @@ import ReactDOM from "react-dom"
 //     <li>Phillip</li>
 // </ul>
 //////////////////////////////////////////////
-const skills = [
-    ["HTML", 10],
-    ["CSS", 7],
-    ["JS", 9],
-    ["react", 8]
+// const skills = [
+//     ["HTML", 10],
+//     ["CSS", 7],
+//     ["JS", 9],
+//     ["react", 8]
+// ]
+// const Skill=({skill:[tech,level]})=>{
+//     return <li key={level}>{tech} {level}</li>
+// }
+
+// const Skills = ({ skills }) => {
+//     console.log(skills)
+//     const list = skills.map((skill) => <Skill skill={skill} />)
+//     return <ul>{list}</ul>
+// }
+
+// const App = () => {
+//     return (
+//         <div class="container">
+//             <div>
+//                 <h1>Skills Level</h1>
+//                 <Skills skills={skills} />
+//             </div>
+//         </div>
+//     )
+// }
+
+import React from "react"
+import ReactDOM from "react-dom"
+
+const countries = [
+    { name: 'Finland', city: 'Helsinki' },
+    { name: 'Sweden', city: 'Stockholm' },
+    { name: 'Denmark', city: 'Copenhagen' },
+    { name: 'Norway', city: 'Oslo' },
+    { name: 'Iceland', city: 'Reykjavík' },
 ]
-const Skill=({skill:[tech,level]})=>{
-    return <li>{tech} {level}</li>
-}
 
-const Skills = ({ skills }) => {
-    console.log(skills)
-    const list = skills.map((skill) => <Skill skill={skill} />)
-    return <ul>{list}</ul>
-}
-
-const App = () => {
+// Country component
+const Country = ({ country: { name, city } }) => {
     return (
-        <div class="container">
-            <div>
-                <h1>Skills Level</h1>
-                <Skills skills={skills} />
-            </div>
-        </div>
+      <div>
+        <h1>{name}</h1>
+        <small>{city}</small>
+      </div>
     )
-}
-
-
-
+  }
+  
+  // countries component
+  const Countries = ({ countries }) => {
+    console.log(countries)
+    const countryList = countries.map((country) => (
+      <Country key={country.name} country={country} />
+    ))
+    return <div>{countryList}</div>
+  }
+  const App = () => (
+    <div className='container'>
+      <div>
+        <h1>Countries List</h1>
+        <Countries countries={countries} />
+      </div>
+    </div>
+  )
 
 const rootElement = document.getElementById("root")
 ReactDOM.render(<App />, rootElement)
