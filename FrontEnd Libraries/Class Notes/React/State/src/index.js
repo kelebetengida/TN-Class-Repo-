0aa09d6f23940112413c04e8018857c4782be089
kelebetengida <engida398@gmail.com>
2024-06-04 16:36:07 -0700

    state

--- FrontEnd Libraries/Class Notes/React/State/src/index.js
+++ FrontEnd Libraries/Class Notes/React/State/src/index.js
@@ -31,95 +31,161 @@
 
 //////////////////////////////////////////////////////////////////////////////////////////////
 // Make a single page application which changes the body of the background based on the season of the year(Autumn, Winter, Spring, Summer)
-import React, { Component } from "react"
-import ReactDOM from "react-dom"
+// import React, { Component } from "react"
+// import ReactDOM from "react-dom"
 
 
-class App extends Component{
+// class App extends Component{
 
-    constructor(props){
-        super(props)
-        this.state={
-            season:this.getSeason()
-        } 
-        this.handleChangeSeason=this.handleChangeSeason.bind(this)
-    }
+//     constructor(props){
+//         super(props)
+//         this.state={
+//             season:this.getSeason()
+//         } 
+//         this.handleChangeSeason=this.handleChangeSeason.bind(this)
+//     }
 
-    getSeason(){
-        const month =new Date().getMonth();
-        if(month >=2 && month <=4){
-            return "spring"
-        } else if(month>=5 && month <=7){
-            return "summer"
-        }else if (month>=8 && month <=10){
-            return"Autmun"
-        }else{
-            return "Winter"
-        }
-    }
+//     getSeason(){
+//         const month =new Date().getMonth();
+//         if(month >=2 && month <=4){
+//             return "spring"
+//         } else if(month>=5 && month <=7){
+//             return "summer"
+//         }else if (month>=8 && month <=10){
+//             return"Autmun"
+//         }else{
+//             return "Winter"
+//         }
+//     }
 
-    getBackgroundColor(){
-        const {season}=this.state;
-        switch(season){
-            case "Spring":
-                return "#a8e6cf";
-            case "Summer":
-                return "#ffd700"
-            case "Autumn":
-                return "#ff8c00"
-            case "Winter":
-                return "#add8e6"
-            default:
-                return "#ffffff"
+//     getBackgroundColor(){
+//         const {season}=this.state;
+//         switch(season){
+//             case "Spring":
+//                 return "#a8e6cf";
+//             case "Summer":
+//                 return "#ffd700"
+//             case "Autumn":
+//                 return "#ff8c00"
+//             case "Winter":
+//                 return "#add8e6"
+//             default:
+//                 return "#ffffff"
 
-        }
-    }
+//         }
+//     }
 
-    handleChangeSeason(newSeason){
-        this.setState({season:newSeason})
-    }
+//     handleChangeSeason(newSeason){
+//         this.setState({season:newSeason})
+//     }
 
-    render(){
-        const {season}=this.state
-        const backgroundColor=this.getBackgroundColor()
-        return(
-            <div style={{
-                backgroundColor, 
-                height:"100vh",
-                display:"flex",
-                flexDirection:"column",
-                justifyContent:"center",
-                alignItems:"center",
-                transition: "background-color 0.5s ease-in-out"
-            }}>
-                <h1> Its {season}!</h1>
-                <div>
-                    <button onClick={()=>this.handleChangeSeason("Spring")}>Spring</button>
-                    <button onClick={()=>this.handleChangeSeason("Summer")}>Summer</button>
-                    <button onClick={()=>this.handleChangeSeason("Autmun")}>Autumn</button>
-                    <button onClick={()=>this.handleChangeSeason("Winter")}>Winter</button>
-                </div>
-            </div>
-        )
-
-
-    }
-}
+//     render(){
+//         const {season}=this.state
+//         const backgroundColor=this.getBackgroundColor()
+//         return(
+//             <div style={{
+//                 backgroundColor, 
+//                 height:"100vh",
+//                 display:"flex",
+//                 flexDirection:"column",
+//                 justifyContent:"center",
+//                 alignItems:"center",
+//                 transition: "background-color 0.5s ease-in-out"
+//             }}>
+//                 <h1> Its {season}!</h1>
+//                 <div>
+//                     <button onClick={()=>this.handleChangeSeason("Spring")}>Spring</button>
+//                     <button onClick={()=>this.handleChangeSeason("Summer")}>Summer</button>
+//                     <button onClick={()=>this.handleChangeSeason("Autmun")}>Autumn</button>
+//                     <button onClick={()=>this.handleChangeSeason("Winter")}>Winter</button>
+//                 </div>
+//             </div>
+//         )
 
 
+//     }
+// }
 
-
-const rootElement=document.getElementById("root")
-ReactDOM.render( <App/>, rootElement)
-
-
-
-
-
-
+// const rootElement=document.getElementById("root")
+// ReactDOM.render( <App/>, rootElement)
 
 
 
 
 
 // Make a single page application which change the body of the background based on the time of the day(Morning, Noon, Evening, Night)
+
+
+import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      timeOfDay: this.getTimeOfDay(),
+    };
+    this.handleChangeTimeOfDay = this.handleChangeTimeOfDay.bind(this);
+  }
+
+  getTimeOfDay() {
+    const hour = new Date().getHours();
+    if (hour >= 5 && hour < 12) {
+      return 'Morning';
+    } else if (hour >= 12 && hour < 17) {
+      return 'Noon';
+    } else if (hour >= 17 && hour < 20) {
+      return 'Evening';
+    } else {
+      return 'Night';
+    }
+  }
+
+  getBackgroundColor() {
+    const { timeOfDay } = this.state;
+    switch (timeOfDay) {
+      case 'Morning':
+        return '#FFFAF0'; // Floral White
+      case 'Noon':
+        return '#FFD700'; // Gold
+      case 'Evening':
+        return '#FF6347'; // Tomato
+      case 'Night':
+        return '#191970'; // Midnight Blue
+      default:
+        return '#FFFFFF'; // White
+    }
+  }
+
+  handleChangeTimeOfDay(newTimeOfDay) {
+    this.setState({ timeOfDay: newTimeOfDay });
+  }
+
+  render() {
+    const { timeOfDay } = this.state;
+    const backgroundColor = this.getBackgroundColor();
+
+    return (
+      <div style={{ 
+        backgroundColor, 
+        height: '100vh', 
+        display: 'flex', 
+        flexDirection: 'column',
+        justifyContent: 'center', 
+        alignItems: 'center', 
+        transition: 'background-color 0.5s ease-in-out'
+      }}>
+        <h1>It's {timeOfDay}!</h1>
+        <div>
+          <button onClick={() => this.handleChangeTimeOfDay('Morning')}>Morning</button>
+          <button onClick={() => this.handleChangeTimeOfDay('Noon')}>Noon</button>
+          <button onClick={() => this.handleChangeTimeOfDay('Evening')}>Evening</button>
+          <button onClick={() => this.handleChangeTimeOfDay('Night')}>Night</button>
+        </div>
+      </div>
+    );
+  }
+}
+
+const rootElement = document.getElementById('root');
+ReactDOM.render(<App />, rootElement);
