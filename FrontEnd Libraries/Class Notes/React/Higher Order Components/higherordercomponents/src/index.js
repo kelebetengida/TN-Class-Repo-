359d26d57c8d9c854cb51779fb3e1804b10cfcbf
kelebetengida <engida398@gmail.com>
2024-06-13 17:44:05 -0700

    Notes

---
+++ FrontEnd Libraries/Class Notes/React/Higher Order Components/higherordercomponents/src/index.js
@@ -0,0 +1,17 @@
+import React from "react"
+import ReactDOM from "react-dom"
+
+//this is a general syntax of a higher order component
+
+// const higherOrderComponent=(Components)=>{
+//   return(props)=>{
+//     return <Component {...props}/>
+//   }  
+// }
+
+
+
+
+const rootElement=document.getElementById("root")
+
+ReactDOM.render(<higherOrderComponent/>, rootElement)
